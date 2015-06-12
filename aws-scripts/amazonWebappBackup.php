7e414669789a4afc189780cc8bb9575330036be5
Alvin Yeoh <alvin.yeoh@sg.gumi.lan>
2015-06-12 09:51:39 +0800

    removal of region

--- aws-scripts/amazonWebappBackup.php
+++ aws-scripts/amazonWebappBackup.php
@@ -1,9 +1,4 @@
-<?php
-    require('vendor/autoload.php');
-    use Aws\Ec2\Ec2Client;
-    use Aws\Ec2\Exception\Ec2Exception;
-    date_default_timezone_set('UTC');
-    
+<?php    
     /************************
      *  Settings
      ************************/	
@@ -12,17 +7,18 @@
     // False for production 
     $dry_run = false;
     
-    // Instance IDs
-    $backup_instances = array(
-        'i-12345' => 'Hello-World',
-        'i-54321' => 'Name-123',
-    );
+    // Prerequisite
+    require('vendor/autoload.php');
+
+    use Aws\Ec2\Ec2Client;
+    use Aws\Ec2\Exception\Ec2Exception;
+    date_default_timezone_set('UTC');    
     
-    // Regions to create images
-    // Refer to http://docs.aws.amazon.com/general/latest/gr/rande.html for region list
-    // Asia Pacific (Singapore) => 'ap-southeast-1'
-    $regions = array(
-        'us-west-1'
+    // Instance IDs
+    
+    $backup_instances = array(
+        'i-12345' => 'GL-DEV-RPT',
+        'i-54321' => 'GL-WEBAPP11'
     );
     
     // Recipient of email notification
@@ -45,57 +41,47 @@
     // Create AMI
     $success = array();
     $failures = array();
-    $successCount = 0;
-    $failureCount = 0;
     
-    foreach($regions as $region) {
-    
-    	foreach($backup_instances as $id => $name) {
-    		
-    		$ami_name = $name.'_'.date('d-m-Y');
-    		
-    		try {
-            	$ec2Client->createImage(array(
-                    // Set to true for testing
-                    'DryRun' => $dry_run,
-                    // InstanceId is required
-                    'InstanceId' => $id,
-                    // Name is required
-                    'Name' => $ami_name,
-                    'NoReboot' => true,
-                    'region' => $region
-            	));
-            	
-            	$success[$region][$id] = $ami_name;
-            	$successCount++;
-    		}
-    		catch(Ec2Exception $e) {
-    			//http://docs.aws.amazon.com/AWSEC2/latest/APIReference/errors-overview.html
-    			$failures[$region][$name.' / '.$id] = $e->getExceptionCode(); 
-    			$failureCount++;
-    		}
-    		
-    		// Pausing 2s
-    		sleep(2);
-    	}
-    }
+	foreach($backup_instances as $id => $name) {
+		
+		$ami_name = $name.'_'.date('d-m-Y');
+		
+		try {
+        	$ec2Client->createImage(array(
+                // Set to true for testing
+                'DryRun' => $dry_run,
+                // InstanceId is required
+                'InstanceId' => $id,
+                // Name is required
+                'Name' => $ami_name,
+                'NoReboot' => true
+        	));
+        	
+        	$success[$id] = $ami_name;
+		}
+		catch(Ec2Exception $e) {
+			//http://docs.aws.amazon.com/AWSEC2/latest/APIReference/errors-overview.html
+			$failures[$name.' / '.$id] = $e->getExceptionCode(); 
+		}
+		
+		// Pausing 2s
+		sleep(2);
+	}
     
     // Send report
     $subject = 'Automated AMI Creation Report - '.date('d M Y');
     
-    $message = 'AMI creation success(es): '.$successCount;
+    $message = 'AMI creation success(es): '.count($success);
     $message .= '<br/>';
-    $message .= 'AMI creation failure(es): '.$failureCount;
+    $message .= 'AMI creation failure(es): '.count($failures);
     
     if(count($success)) {
     
     	$message .= '<br/><br/>';		
     	$message .= "AMI(s) created: <br/>";
     	
-    	foreach($success as $region => $data) {
-    		foreach($data as $id => $name) {
-    			$message .= $region.' => '.$id.' => '.$name.'<br/>';
-    		}
+    	foreach($success as $id => $name) {
+		  $message .= $id.' => '.$name.'<br/>';
     	}
     }
     
@@ -104,10 +90,8 @@
         $message .= '<br/><br/>';
         $message .= "AMI(s) creation failures: <br/>";
     
-        foreach($failures as $region => $data) {
-    	    foreach($data as $id => $status) {
-    	        $message .= $region.' => '.$id.' => '.$status.'<br/>';
-    	    }
+        foreach($failures as $id => $status) {
+    	   $message .= $id.' => '.$status.'<br/>';
         }
     }
     
@@ -125,13 +109,13 @@
         $mail->isSMTP();                                      	// Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';  						// Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               	// Enable SMTP authentication
-        $mail->Username = '..ToDo..';                           // SMTP username
-        $mail->Password = '..ToDo..';                           // SMTP password
+        $mail->Username = '..ToDo..';                 	// SMTP username
+        $mail->Password = '..ToDo..';                         // SMTP password
         $mail->SMTPSecure = 'tls';                            	// Enable TLS encryption, `ssl` also accepted
         $mail->Port = 25;                                    	// TCP port to connect to
         
         $mail->From = '..ToDo..';
-        $mail->FromName = 'GumiLive Tools';
+        $mail->FromName = '..ToDo..';
         
         foreach($recipient as $email)
             $mail->addAddress($email);
